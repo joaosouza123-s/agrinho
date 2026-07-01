534ee6b9d93c3272d6ee91863e90964829c91158
joaosouza123-s <santos.souza.joao3001@escola.pr.gov.br>
2026-07-01 17:20:46 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function mostrarMensagem() {
+    document.getElementById("mensagem").innerHTML =
+    "💙 Todos merecem respeito. Se você presencia ou sofre bullying, procure um adulto de confiança e peça ajuda.";
+}
